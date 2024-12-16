aebddc8f4141b2275a256f4a9eb27b151fead5de
virtualabishek <717abishekneupane@gmail.com>
2024-12-16 13:44:47 +0545

    Will back, after completing TailwindCSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import mainPic from "../assets/images/brother.jpg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex">
+        <h1 className="flex-auto text-lg text-balance">
+          Hi! I am Abinash Neupane.
+        </h1>
+        <img
+          src={mainPic}
+          alt="Profile Picture"
+          className="flex-auto items-end"
+        />
+      </div>
+    </>
+  );
 };
 
 export default Home;
