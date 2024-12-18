86a028314b76132191a7b7a276796e8cea436ade
virtualabishek <717abishekneupane@gmail.com>
2024-12-18 13:40:00 +0545

    Just for fun

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,7 @@ import mainPic from "../assets/images/brother.jpg";
 const Home = () => {
   return (
     <>
-      <div className="flex">
-        <h1 className="flex-auto text-lg text-balance">
-          Hi! I am Abinash Neupane.
-        </h1>
-        <img
-          src={mainPic}
-          alt="Profile Picture"
-          className="flex-auto items-end"
-        />
-      </div>
+      <div className="h-screen w-full bg-hero-pattern bg-cover bg-center flex items-center justify-center"></div>
     </>
   );
 };
